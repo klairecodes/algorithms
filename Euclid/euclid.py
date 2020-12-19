83e11b6156454d9a19cf4b7c03a236c6e5098fe0
Klaus Curde <kcurde@gmail.com>
2020-12-19 14:28:37 -0500

    clean up and comment euclid, added gitignore

--- Euclid/euclid.py
+++ Euclid/euclid.py
@@ -1,16 +1,21 @@
 import sys
 
+"""
+In mathematics, the Euclidean algorithm, or Euclid's algorithm, is an efficient 
+method for computing the greatest common divisor of two integers, the largest 
+number that divides them both without a remainder.
+Source: https://en.wikipedia.org/wiki/Euclidean_algorithm
+"""
 def euclid(x, y):
     if y == 0: return x
     a = max(x, y)
     b = min(x, y)
-    gcf = 0
 
     while b != 0:
         if a > b:
             a -= b
         else:
-            b = b - a
+            b -= a
     return a
             
     
